bb2e643f0f755e682e49e02f2eb1da7938bde834
MasterBowtie <greenducky12@hotmail.com>
2023-02-04 18:33:11 -0700

    Improved HTML cleaning, ready for JSON building

--- src/tools/getHTML.py
+++ src/tools/getHTML.py
@@ -1,27 +1,62 @@
 import json
-
+import sys
 import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse, urljoin
+
+def buildJson(array):
+    pass
+
+def discectHTML(html):
+    html = html.replace('\ue50e', "").replace('\u2011', "")
+    file = html.split("\n")
+    write = False
+    file2 = []
+
+    # filter lines...
+    for line in file:
+        if "id=\"main\"" in line:
+            write = True
+        if write and "</div>" in line:
+            write = False
+        if "" != line.strip() and write:
+            file2.append(line.strip())
+
+    string = ""
+    for line in file2:
+        line = line.replace("<span", "\n<span").replace("</span>", "\n</span>\n")
+        line = line.replace("alt=", "\nalt=").replace("<b", "\n<b").replace("</b>", "</b>\n")
+        line  = line.replace("</h3>", "</h3>\n").replace("</h2>", "</h2>\n").replace("</h1>", "</h>\n")
+        string += line.replace("<h", "\n<h")
+    file = string.split("\n")
+
+    newText = ""
+    for line in file:
+        if "span" in line or "" == line or "div" in line:
+           pass
+        else:
+            write = True
+            for char in line:
+                if char == "<":
+                    write = False
+                if write:
+                    newText += char
+                if char == ">":
+                    write = True
+            newText += "\n"
+    file = newText.split("\n")
+    while True:
+        if "alt=" in file[0]:
+            file.pop(0)
+            break
+        file.pop(0)
+    count = 0
+    for line in file:
+        print(f"{count} {line}")
+        count += 1
 
 
 
-
-def setUPJSON(param):
-    param["source"] = ""
-    param["trait"] = {}
-    param["languages"] = []
-    param["addLanguages"] = []
-    param["boosts"] = []
-    param["flaws"] = []
-
-
-if __name__ == "__main__":
-    baseURL = "https://2e.aonprd.com/"
-    url = "Ancestries.aspx?ID=1"
-    response = requests.get(f"{baseURL}{url}")
-    html = response.text
-    html.replace('\ue50e', "")
+def discectHTML1(html, url):
+    html = html.replace('\ue50e', "").replace('\u2011', "")
     file = open("temp.txt", mode="w")
     print(html, file=file)
     file.close()
@@ -44,6 +79,7 @@ if __name__ == "__main__":
         line = file.readline()
 
     newText += "\neof\n"
+
     print(newText.replace("<", "\n").replace(">", "\n"), file=file2)
     file.close()
     file2.close()
@@ -53,7 +89,7 @@ if __name__ == "__main__":
     newText = ""
     line = file.readline().strip()
     while "eof" not in line:
-        if len(line.strip()) == 0 or line.startswith("/") or line.startswith("h2") or line.startswith("br")\
+        if len(line.strip()) == 0 or line.startswith("/") or line.startswith("h2") or line.startswith("br") \
                 or line.startswith("img") or line.startswith(","):
             line = file.readline()
             continue
@@ -61,6 +97,8 @@ if __name__ == "__main__":
             newText += line
             line = file.readline()
     newText += "\neof\n"
+    if "ID=6" in url:
+        print(newText)
     print(newText, file=file2)
     file.close()
     file2.close()
@@ -81,7 +119,7 @@ if __name__ == "__main__":
                 dict[name] = {}
                 file2.seek(0)
                 found = True
-                setUPJSON(dict[name])
+                __setUPJSON(dict[name])
             line = file2.readline()
 
         if "Source" == line.strip():
@@ -118,9 +156,14 @@ if __name__ == "__main__":
 
         if "Ability Boosts" == line.strip():
             line = file2.readline()
-            while line.strip() != "Ability Flaw(s)":
-                dict[name]["boosts"].append(line.strip())
+            if line.strip() == "Two free ability boosts":
+                dict[name]["boosts"].append("Free")
+                dict[name]["boosts"].append("Free")
                 line = file2.readline()
+            else:
+                while line.strip() != "Ability Flaw(s)":
+                    dict[name]["boosts"].append(line.strip())
+                    line = file2.readline()
 
         if "Ability Flaw(s)" == line.strip():
             line = file2.readline()
@@ -154,11 +197,45 @@ if __name__ == "__main__":
         dict[name]["vision"] = "Normal"
     newText += "\neof\n"
 
-    print(newText)
+    #print(newText)
     file = open(f"{name}.json", mode="w")
-    string = json.dumps(dict)
-    print(json.dumps(dict, indent=2), file=file)
+    string = json.dumps(dict, indent=2)
+    #print(string)
+    print(string, file=file)
     file.close()
+
+def __setUPJSON(param):
+    param["source"] = ""
+    param["trait"] = {}
+    param["languages"] = []
+    param["addLanguages"] = []
+    param["boosts"] = []
+    param["flaws"] = []
+    param["vision"] = {}
+    param["extras"] = {}
+
+
+if __name__ == "__main__":
+    baseURL = "https://2e.aonprd.com/"
+    count = 1
+    accepted = []
+    failed = []
+    while count < 11:
+        url = f"Ancestries.aspx?ID={count}"
+        try:
+            response = requests.get(f"{baseURL}{url}")
+            if not response.ok:
+                failed.append(f"{baseURL}{url}")
+                print(f"Error: crawl({baseURL}{url}) {response.status_code} {response.reason}", file=sys.stderr)
+            else:
+                print(f"Accepted:{baseURL}{url}")
+                accepted.append(f"{baseURL}{url}")
+                discectHTML(response.text)
+        except Exception as e:
+            print(f"Error: {baseURL}{url} is not accessible because {e}", file=sys.stderr)
+
+        count += 1
+
 
 
 
